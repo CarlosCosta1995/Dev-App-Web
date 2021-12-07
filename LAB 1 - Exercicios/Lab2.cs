4f1e26cb09e4622279de2c1f8fe41edcd84a5284
Carlos Costa <andrefr1995@gmail.com>
2021-12-07 20:36:17 +0000

    ok

--- LAB 1 - Exercicios/Lab2.cs
+++ LAB 1 - Exercicios/Lab2.cs
@@ -217,3 +217,107 @@ while (i >= 0)
 
 }
 ---------------------------------------------------------------------*/
+
+//========================================== Extras e/ou correções ==================================
+/*Exercicio 1
+Pretende - se implementar uma função que utilize o algoritmo de Euclides para calcular o máximo divisor comum de dois números inteiros. 
+O programa deve pedir ao utilizador para inserir os números e no final deve apresentar o resultado.
+O algoritmo de Euclides para encontrar o MDC de quaisquer números inteiros A e B: 
+Repetir:
+Se B é zero, o MDC é A.
+Caso contrário:
+Atribuir o valor do B ao R (r=b)
+Calcular o resto da divisão inteira de A por B e atribuir a B (b=a%b)
+Atribuir o valor de R ao A (a=r)
+Retornar A(=MDC);
+
+//Inserir os dois numeros pedidos
+Console.WriteLine("Insira o valor para a");
+int a = int.Parse(Console.ReadLine()); //guarda o valor inserido em a
+
+Console.WriteLine("Insira o valor para b");
+int b = int.Parse(Console.ReadLine()); //guarda o valor inserido em b
+
+double rx; //variaveil auxiliar
+
+if (b == 0)
+{
+    Console.WriteLine("O MDC é" + a);
+}
+else
+{
+    b = rx;
+    b = a % b;
+    a = rx;
+    
+    Console.WriteLine("O MDC é" + a);
+}
+------------------------------------------------------------------------------------*/
+
+/* Exercicio 2 errado???
+//Implemente uma função que faz a conversão de temperatura entre Fahrenheit e graus Celsius. 
+//Faça todas as alterações propostas nos slides das aulas teóricas e verifique os resultados.
+//Para converter Fahrenheit to Celsius, subtraia 32 e multiplique por 5/9
+
+//inserir graus Celsius
+Console.WriteLine("Insira a temperatura actual em graus Fahrenheit.");
+double fahrenheit = double.Parse(Console.ReadLine()); //guardas o numero inserido com 2 casas decimais
+
+double celsius ;
+
+celsius = (fahrenheit - 32) * (5/9);
+Console.WriteLine("Neste momento está" + celsius + "graus Celsius");
+------------------------------------------------------------------------------------*/
+
+
+/* Exercicio 3
+// Escreva uma função que pede um número inteiro ao utilizador, testa se este é par ou impar e retorna o resultado.
+Console.WriteLine("Insira um numero para verificar se é par ou impar");
+int i = 0;
+while(i == 0)
+{
+    int num = int.Parse(Console.ReadLine());
+    int resto = 0;
+    resto = num % 2;
+
+    if (resto == 0)
+    {
+        Console.WriteLine("numero " + num + " é par.");
+    }
+    else
+    {
+        Console.WriteLine("numero " + num + " é impar.");
+    }
+}------------------------------------------------------------------------------------*/
+
+
+/*Exercicio 6
+//Escreva uma função que devolve todos os números múltiplos de 19 entre 19 e 2000.
+
+
+int iterador = 1; //variavel iterador para a funcao while
+
+int numR = 19; //multiplicador parte da tabuada do 19
+float result = 0;
+
+int x = 0; // ver quantos numeros tem entre 19 e 2000
+x = 2000 - 19;
+
+
+while (iterador <= x) // colocar um limite no iterador 
+{
+    result = numR%19;
+
+    if (result == 0) 
+    {
+        Console.WriteLine("Este número "+ numR + " é multiplo de 19.");
+        
+    }
+    // else
+    //{
+    //   Console.WriteLine("Não existem mais multiplos de 19 entre 19 e 2000");
+    //}
+    //Console.WriteLine(iterador);
+    numR++;
+    iterador++;
+}---------------------------------------------------------------------*/
--- LAB 1 - Exercicios/Lab3.cs
+++ LAB 1 - Exercicios/Lab3.cs
@@ -3,110 +3,8 @@
 
 //======================================= LAB 3 ===============================
 
+
 /*Exercicio 1
-Pretende - se implementar uma função que utilize o algoritmo de Euclides para calcular o máximo divisor comum de dois números inteiros. 
-O programa deve pedir ao utilizador para inserir os números e no final deve apresentar o resultado.
-O algoritmo de Euclides para encontrar o MDC de quaisquer números inteiros A e B: 
-Repetir:
-Se B é zero, o MDC é A.
-Caso contrário:
-Atribuir o valor do B ao R (r=b)
-Calcular o resto da divisão inteira de A por B e atribuir a B (b=a%b)
-Atribuir o valor de R ao A (a=r)
-Retornar A(=MDC);
-
-//Inserir os dois numeros pedidos
-Console.WriteLine("Insira o valor para a");
-int a = int.Parse(Console.ReadLine()); //guarda o valor inserido em a
-
-Console.WriteLine("Insira o valor para b");
-int b = int.Parse(Console.ReadLine()); //guarda o valor inserido em b
-
-double rx; //variaveil auxiliar
-
-if (b == 0)
-{
-    Console.WriteLine("O MDC é" + a);
-}
-else
-{
-    b = rx;
-    b = a % b;
-    a = rx;
-    
-    Console.WriteLine("O MDC é" + a);
-}
-------------------------------------------------------------------------------------*/
-
-/* Exercicio 2 errado???
-//Implemente uma função que faz a conversão de temperatura entre Fahrenheit e graus Celsius. 
-//Faça todas as alterações propostas nos slides das aulas teóricas e verifique os resultados.
-//Para converter Fahrenheit to Celsius, subtraia 32 e multiplique por 5/9
-
-//inserir graus Celsius
-Console.WriteLine("Insira a temperatura actual em graus Fahrenheit.");
-double fahrenheit = double.Parse(Console.ReadLine()); //guardas o numero inserido com 2 casas decimais
-
-double celsius ;
-
-celsius = (fahrenheit - 32) * (5/9);
-Console.WriteLine("Neste momento está" + celsius + "graus Celsius");
-------------------------------------------------------------------------------------*/
-
-
-/* Exercicio 3
-// Escreva uma função que pede um número inteiro ao utilizador, testa se este é par ou impar e retorna o resultado.
-Console.WriteLine("Insira um numero para verificar se é par ou impar");
-int i = 0;
-while(i == 0)
-{
-    int num = int.Parse(Console.ReadLine());
-    int resto = 0;
-    resto = num % 2;
-
-    if (resto == 0)
-    {
-        Console.WriteLine("numero " + num + " é par.");
-    }
-    else
-    {
-        Console.WriteLine("numero " + num + " é impar.");
-    }
-}------------------------------------------------------------------------------------*/
-
-
-/*Exercicio 6
-//Escreva uma função que devolve todos os números múltiplos de 19 entre 19 e 2000.
-
-
-int iterador = 1; //variavel iterador para a funcao while
-
-int numR = 19; //multiplicador parte da tabuada do 19
-float result = 0;
-
-int x = 0; // ver quantos numeros tem entre 19 e 2000
-x = 2000 - 19;
-
-
-while (iterador <= x) // colocar um limite no iterador 
-{
-    result = numR%19;
-
-    if (result == 0) 
-    {
-        Console.WriteLine("Este número "+ numR + " é multiplo de 19.");
-        
-    }
-    // else
-    //{
-    //   Console.WriteLine("Não existem mais multiplos de 19 entre 19 e 2000");
-    //}
-    //Console.WriteLine(iterador);
-    numR++;
-    iterador++;
-}---------------------------------------------------------------------*/
-
-/*Exercicio 8
 //Escreva um programa que pede ao utilizador para inserir o nome e idade dos seus amigos (número definido pelo utilizador)
 //e apresenta quem é o mais velho e quem é mais novo.
 
@@ -154,9 +52,11 @@ while (i >=0)
         i = -1; 
     }
     
-}------------------------------------------------------------------*/
+}*/
 
-/* Exercicio 9
+//-----------------------------------------------------------------------------//
+
+/* Exercicio 2
 //Complemente o programa anterior para ter em conta o mês de nascimento no cálculo.
 
 string nome = "joao";
@@ -215,9 +115,11 @@ while (i >= 0)
     {
         i = -1;
     }
-}-----------------------------------------------------------------------------*/
+}*/
 
-/* Exercicio 10
+//-----------------------------------------------------------------------------//
+
+/* Exercicio 3
 //Escreva um programa que peça a um utilizador para inserir o número de elementos da turma.
 //Peça ao utilizador para introduzir a idade dos mesmos e imprima a média de idade da turma
  
@@ -258,11 +160,50 @@ while(i >= 0)
         Console.WriteLine("A média de idades dos {0} é de {1}.", numero_elemento, media_idades);
         i = -1;
     }
-}-----------------------------------------------------------------------------*/
+}*/
 
-// Exercicio 11
+//-----------------------------------------------------------------------------//
+
+// Exercicio 4
 //Escreva um programa que peça ao utilizador para inserir a nota (de 0 a 20)
 //de 30 alunos de uma turma e imprime qual é a nota mais baixa, a nota mais
 //alta e a média das notas atribuídas.
 
+Console.WriteLine("Insira uma nota:");
+int nota = 0;
 
+int numeroAlunos = 30;
+
+int notaMax = 0;
+int notaMin = 0;
+
+int iterador = 0;
+
+while (iterador <= numeroAlunos)
+{
+    nota = int.Parse(Console.ReadLine());
+
+    if (iterador == 0 && nota >= 0 && nota <= 20)
+    {
+        notaMin = nota;
+        Console.WriteLine(" Nota minima {0}", notaMin);
+        iterador++;
+    }
+    if (iterador > 0 && nota > notaMax && nota >= 0 && nota <= 20)
+    {
+        notaMax = nota;
+        Console.WriteLine(" Nota maxima {0}", notaMax);
+        iterador++;
+    }
+    if (iterador > 0 && nota < notaMin && nota >= 0 && nota <= 20)
+    {
+        notaMin = nota;
+        Console.WriteLine(" Nota minima {0}", notaMin);
+        iterador++;
+    }
+    if (nota < 0 && nota > 20)
+    {
+        Console.WriteLine("Nota invalida, apenas podem ser valores inteiros entre 0 a 20.\n Nota inserida {0}", nota);
+        break;
+    }
+}
