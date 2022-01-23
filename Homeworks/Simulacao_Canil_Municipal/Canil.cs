716c2f7a2bfa10453ee5eed57ba0ba8d43d9fe80
Carlos Costa <andrefr1995@gmail.com>
2022-01-23 23:05:12 +0000

    Canil TPC

---
+++ Homeworks/Simulacao_Canil_Municipal/Canil.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulacao_Canil_Municipal
+{
+    //Dogs section
+    public class Dog
+    {
+        private string[] dog  = new string[5];
+
+        public string[] Doge 
+        { 
+            get { return dog; }
+            set { dog = value; }
+        } 
+
+        public Array AddDog(string dogName)
+        {
+            dog = new string[] { dogName };
+            return dog;
+        }
+
+        public Array AdoptDog(int escolherCao)
+        {
+            Console.WriteLine("Adoção do cao previamente chamado de {0}", dog[escolherCao].ToString());
+            dog[escolherCao] = "";
+            return dog;
+        }
+    }
+
+    //Cat section
+    public class Cat
+    {
+        private string[] cat = new string[2];
+
+        public Array addCat(string dogName)
+        {
+            if (cat.Length <= 2)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    cat[i] = dogName;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Canil dos gatos cheio!");
+            }
+            return cat;
+        }
+
+        public Array AdoptDog(int escolherCao)
+        {
+            Console.WriteLine("Adoção do gato previamente chamado de {0}", cat[escolherCao].ToString());
+            cat[escolherCao] = "";
+            return cat;
+        }
+    }
+}
---
+++ Homeworks/Simulacao_Canil_Municipal/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Implementar um programa que simule o canil municipal.
+• Limite de animais a acolher (considerar apenas cães e gatos)
+• Permitir registar um animal acolhido
+• Permitir registar a adoção de um animal */
+
+using Simulacao_Canil_Municipal;
+
+Dog cao = new Dog();
+
+cao.AddDog("Farrusco");
+//Console.WriteLine(cao.Doge[0]);
+cao.AddDog("Bobby");
+Console.WriteLine(cao.Doge[0]);
+Console.WriteLine(cao.Doge[1]);
+
+/*
+for (int i = 0; i <= 2; i++)
+{
+    Console.WriteLine(cao.Doge[i]);
+}*/
+
