a02fc3190b221c21e396e2fe4cbf25074bb80461
Carlos Costa <andrefr1995@gmail.com>
2022-02-24 16:27:41 +0000

    training UML

---
+++ Homeworks/UML/Uml/UML Trainning/Movable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_Trainning
+{
+    public interface IMovable
+    {
+        public void moveUp();
+        public void moveDown();
+        public void moveLeft();
+        public void moveRight();
+    }
+
+    public class MovablePoint : IMovable
+    {
+        public void moveDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void moveLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void moveRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void moveUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
