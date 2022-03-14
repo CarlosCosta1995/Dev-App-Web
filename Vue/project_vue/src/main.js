282fc7043d72a110cdc7b47bd60e46120af42df2
Carlos Costa <andrefr1995@gmail.com>
2022-03-14 15:11:34 +0000

    Project vue
    erro na const app do firebase

---
+++ Vue/project_vue/src/main.js
@@ -0,0 +1,27 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+
+createApp(App).use(router).mount('#app')
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA-pSG7QUfcxU5uFar8IYX9Ff8BUJv3QQQ",
+  authDomain: "project-vue-cc.firebaseapp.com",
+  projectId: "project-vue-cc",
+  storageBucket: "project-vue-cc.appspot.com",
+  messagingSenderId: "249136196491",
+  appId: "1:249136196491:web:5edb383d5e84fa03351d8e",
+  measurementId: "G-0CMQ1F3ZJC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
