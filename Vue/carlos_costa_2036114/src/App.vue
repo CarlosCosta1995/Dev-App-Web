2628ddf94f01f7b9f49d8174f78e1f69cf91a53f
Carlos Costa <andrefr1995@gmail.com>
2022-03-17 19:12:08 +0000

    boostrap insalado

---
+++ Vue/carlos_costa_2036114/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <div>
+        <b-navbar type="dark" variant="dark">
+          <b-navbar-nav>
+            <b-nav-item ><router-link to="/">Home</router-link></b-nav-item>
+            <b-nav-item ><router-link to="/about">About</router-link></b-nav-item>
+
+            <!-- Navbar dropdowns -->
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown text="User" right>
+              <b-dropdown-item href="#">Account</b-dropdown-item>
+              <b-dropdown-item href="#">Settings</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-navbar>
+      </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+/*
+https://bootstrap-vue.org/docs/components/carousel
+https://bootstrap-vue.org/docs
+*/
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
